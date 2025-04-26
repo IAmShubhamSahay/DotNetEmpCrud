0507cc8ae8b3c3abcc58d07df66de6bfab1f6988
IAmShubhamSahay <iamshubhamsahay@gmail.com>
2025-04-26 23:20:02 +0530

    Required

--- EmployeeCrudApp/EmployeeCrudApp/Models/Employee.cs
+++ EmployeeCrudApp/EmployeeCrudApp/Models/Employee.cs
@@ -1,11 +1,21 @@
-﻿public class Employee
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Employee
 {
+    [Required]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Gender { get; set; }
+    [Required]
     public string State { get; set; }
+    [Required]
     public string Address { get; set; }
+    [Required]
     public string Phone { get; set; }
+    [Required]
     public decimal Salary { get; set; }
+    [Required]
     public string Email { get; set; }
 }
