a8880a4bd0b7721ec04ce008f76b4c2a8ea77b4e
IAmShubhamSahay <iamshubhamsahay@gmail.com>
2025-04-26 22:44:17 +0530

    Initial commit - Added project excluding VS files

---
+++ EmployeeCrudApp/EmployeeCrudApp/Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using EmployeeCrudApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+public class EmployeeController : Controller 
+{
+    private readonly AppDbContext _context;
+    public EmployeeController(AppDbContext context )
+    {
+        _context = context;
+    }
+    private List<string> GetStates()
+    {
+        return new List<string> {
+            "Uttar Pradesh", "Maharashtra", "Delhi", "Haryana", "Punjab",
+            "Karnataka", "Tamil Nadu", "Rajasthan", "Gujarat", "Bihar",
+            "Madhya Pradesh", "West Bengal", "Kerala", "Odisha", "Assam"
+        };
+    }
+
+    public IActionResult Index()
+    {
+        var employees = _context.Employees.ToList();
+        return View(employees);
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.States = new SelectList(GetStates());
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Employee employee)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        ViewBag.States = new SelectList(GetStates());
+        return View(employee);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var employee = _context.Employees.Find(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        ViewBag.States = new SelectList(GetStates());
+        return View(employee);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Employee employee)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Employees.Update(employee);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        ViewBag.States = new SelectList(GetStates());
+        return View(employee);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var employee = _context.Employees.Find(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        return View(employee);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var employee = _context.Employees.Find(id);
+        if (employee == null)
+        {
+            return NotFound();
+        }
+        _context.Employees.Remove(employee);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+}
+
---
+++ EmployeeCrudApp/EmployeeCrudApp/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string State { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public decimal Salary { get; set; }
+    public string Email { get; set; }
+}
